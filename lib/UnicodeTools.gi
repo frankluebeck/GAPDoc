frankluebeck/GAPDoc

commit a30e73c28bb80993cc95ab7bf476cb3bf099d550

interpret encoding in several places case insensitively.
git-svn-id: path/gapdoc/trunk@375

lib/UnicodeTools.gi
@@ -61,7 +61,22 @@ UNICODE_RECODE.f := function()
 
   UNICODE_RECODE.NormalizedEncodings.("UTF-8") := "UTF-8";
   UNICODE_RECODE.NormalizedEncodings.("XML") := "XML";
+  for nam in RecFields(UNICODE_RECODE.NormalizedEncodings) do
+    UNICODE_RECODE.NormalizedEncodings.(LowercaseString(nam)) :=
+              UNICODE_RECODE.NormalizedEncodings.(nam);
+  od;
 end;
+# can be used for case insensitive normalization
+UNICODE_RECODE.NormalizedEncoding := function(str)
+  local lstr;
+  lstr := LowercaseString(str);
+  if not IsBound(UNICODE_RECODE.NormalizedEncodings.(lstr)) then
+    return fail;
+  else
+    return UNICODE_RECODE.NormalizedEncodings.(lstr);
+  fi;
+end;
+
 UNICODE_RECODE.f();
 Unbind(UNICODE_RECODE.f);
 # slightly more efficient for latin1:

lib/BibXMLextTools.gi
@@ -404,11 +404,11 @@ InstallGlobalFunction(StringBibAsXMLext,  function(arg)
       enc := "UTF-8";
     fi;
   fi;
-  if not IsBound(UNICODE_RECODE.NormalizedEncodings.(enc)) then
+  if UNICODE_RECODE.NormalizedEncodings(enc) = fail then
     Info(InfoBibTools, 1, "don't know encoding ", enc, " using ISO-8859-1\n");
     enc := "ISO-8859-1";
   else
-    enc := UNICODE_RECODE.NormalizedEncodings.(enc);
+    enc := UNICODE_RECODE.NormalizedEncoding(enc);
   fi;
   if enc <> "UTF-8" then
     r := ShallowCopy(r);

lib/HelpBookHandler.g
@@ -304,8 +304,8 @@ HELP_BOOK_HANDLER.GapDocGAP.HelpData := function(book, entrynr, type)
     else
       outenc := "ISO-8859-1";
     fi;
-    enc := UNICODE_RECODE.NormalizedEncodings.(enc);
-    outenc := UNICODE_RECODE.NormalizedEncodings.(outenc);
+    enc := UNICODE_RECODE.NormalizedEncoding(enc);
+    outenc := UNICODE_RECODE.NormalizedEncoding(outenc);
     if enc <> outenc then
       str := Unicode(str, enc);
       if outenc = "ISO-8859-1" then

lib/XMLParser.gi
@@ -463,10 +463,10 @@ InstallGlobalFunction(GetElement, function(str, pos)
           tmp := GetElement(tmp, 3);
           if IsBound(tmp.attributes.encoding) then
             tmp := tmp.attributes.encoding;
-            if not IsBound(UNICODE_RECODE.NormalizedEncodings.(tmp)) then
+            if UNICODE_RECODE.NormalizedEncoding(tmp) = fail then
               Error("Cannot parse document in encoding ", tmp, "\n");
             fi;
-            XMLPARSERFLAGS.Encoding := UNICODE_RECODE.NormalizedEncodings.(tmp);
+            XMLPARSERFLAGS.Encoding := UNICODE_RECODE.NormalizedEncoding(tmp);
             # if not in UTF-8 encoding we recode rest of the document now
             if XMLPARSERFLAGS.Encoding <> "UTF-8" then
               Info(InfoGAPDoc, 1, "#I recoding input from ",

commit 5dadd8b92c23d09cccd32f48a2e95d2ac3e75401

Fix function definitions
Some unicode decoder functions where installed inside a function and
referred to a local variable that was changed afterwards.

Now decoding should work for latin-X with X > 1 and again for ASCII (this was
broken since commit a30e73c2).

lib/UnicodeTools.gi
@@ -45,19 +45,17 @@ UNICODE_RECODE.NormalizedEncodings := rec(
   percent := "URL",
 );
 UNICODE_RECODE.f := function()
-  local nam, i;
-  for i in Concatenation([1..11],[13..15]) do
-    nam := Concatenation("ISO-8859-",String(i));
+  local f, nam, i;
+  f := function(nam)
     UNICODE_RECODE.NormalizedEncodings.(nam) := nam;
     UNICODE_RECODE.Decoder.(nam) := function(str)
       return UNICODE_RECODE.TABLES.(nam){List(str, INT_CHAR)+1};
     end;
-  od;
-  nam := "ANSI_X3.4-1968";
-  UNICODE_RECODE.NormalizedEncodings.(nam) := nam;
-  UNICODE_RECODE.Decoder.(nam) := function(str)
-    return UNICODE_RECODE.TABLES.(nam){List(str, INT_CHAR)+1};
   end;
+  for i in Concatenation([1..11],[13..15]) do
+    f(Concatenation("ISO-8859-",String(i)));
+  od;
+  f("ANSI_X3.4-1968");
 
   UNICODE_RECODE.NormalizedEncodings.("UTF-8") := "UTF-8";
   UNICODE_RECODE.NormalizedEncodings.("XML") := "XML";